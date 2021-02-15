8ffd4d69db0ad8981506100335a3c9e70022b15b
apple <apple@apples-MacBook-Pro.local>
2021-02-15 03:33:26 +0100

    moved main application to a springboot package and added @ComponentScan annotation

--- src/main/java/com/example/demo/springboot/DemoApplication.java
+++ src/main/java/com/example/demo/springboot/DemoApplication.java
@@ -1,11 +1,13 @@
-package com.example.demo;
+package com.example.demo.springboot;
 
 import com.example.demo.controllers.GreetingController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.demo") //this helps when the program entry point is in a different package, as in this case
 public class DemoApplication {
 
     public static void main(String[] args) {
